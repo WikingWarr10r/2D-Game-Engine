8b22a3733e092e57f491c89585fc15f8c37329b6
WikingWarr10r <110036601+WikingWarr10r@users.noreply.github.com>
2025-09-16 09:38:21 +0100

    Object Spawning Fix
    Objects spawn based on if fast spawning is on and means that only one spawns by default

--- physics_ui.py
+++ physics_ui.py
@@ -4,6 +4,7 @@ import time
 class PhysicsUI:
     def __init__(self, engine, pos):
         self.engine = engine
+        self.held = False
 
         self.ui = UIObject("Physics Editor", pos, 300, 200, engine)
         self.ui.add_label("Engine Variables:")
@@ -19,6 +20,7 @@ class PhysicsUI:
         self.ui.add_spacer()
         self.ui.add_button(False, "Add New Object", True)
         self.ui.add_button(False, "Mouse Mode", False)
+        self.ui.add_button(False, "Fast Spawn", False)
         self.ui.add_spacer()
         
         self.ui.add_label("Simulation:")
@@ -55,17 +57,28 @@ class PhysicsUI:
         
         ui_rect = pygame.Rect(self.ui.pos.x, self.ui.pos.y, self.ui.width, self.ui.height)
 
+        if self.ui.get_value("Fast Spawn"):
+            self.held = False
+
         if self.ui.get_value("Mouse Mode"):
             self.ui.set_value("Position", mouse_pos)
-            if pygame.mouse.get_pressed()[0] and not ui_rect.collidepoint(mouse_pos.x, mouse_pos.y):
+            if pygame.mouse.get_pressed()[0] and not ui_rect.collidepoint(mouse_pos.x, mouse_pos.y) and self.held == False:
                 self.engine.add_object(position, velocity, radius)
+                self.held = True
+            if not pygame.mouse.get_pressed()[0]:
+                self.held = False
             if pygame.mouse.get_pressed()[2]:
                 for obj in self.engine.objects:
                     length = (mouse_pos - obj.pos).length()
                     obj.add_force(((mouse_pos - obj.pos) / vec2(length, length)) * 50)
 
         if self.ui.get_value("Add New Object"):
-            self.engine.add_object(position, velocity, radius)
+            if self.held == False:
+                self.engine.add_object(position, velocity, radius)
+                self.held = True
+        else:
+            if not self.ui.get_value("Mouse Mode"):
+                self.held = False
 
         self.engine.draw_cross(position)
         self.engine.draw_line(position, position + (velocity/vec2(10, 10)), (255, 0, 0, 122))
