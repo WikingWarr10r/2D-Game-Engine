32f72cc0f875c67fe922b2813059bf3b5d9e92d7
WikingWarr10r <110036601+WikingWarr10r@users.noreply.github.com>
2025-09-27 13:22:49 +0100

    Finished Scene Manager

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from engine_core import *
 from physics_ui import PhysicsUI
 from profiler_ui import ProfilerUI
-from scene_manager import SceneManager
+from scene_manager_ui import SceneManagerUI
 
 engine = EngineCore()
 
 ui = PhysicsUI(engine, vec2(950, 30))
 profiler = ProfilerUI(engine, vec2(30, 30))
 
-scene_manager = SceneManager(engine)
-
-scene_manager.load()
-ui.regenerate()
+scene_manager = SceneManagerUI(engine, vec2(30, 625), ui)
 
 while engine.looping:
     start = time.time()
     ui.update_simulation()
     ui.object_spawning()
+    scene_manager.update()
 
     engine.main_loop()
     ui.update_stats(start)
--- scene_manager_ui.py
+++ scene_manager_ui.py
@@ -1,8 +1,20 @@
 from ui_container import *
+from scene_manager import SceneManager
 
 class SceneManagerUI:
-    def __init__(self, engine, pos):
+    def __init__(self, engine, pos, physics_ui):
         self.engine = engine
         self.ui = UIObject("Scene Manager", pos, 300, 200, engine)
+        self.scene_manager = SceneManager(engine)
+        self.physics_ui = physics_ui
+
         self.ui.add_button(False, "Save Scene", True)
         self.ui.add_button(False, "Load Scene", True)
+
+    def update(self):
+        if self.ui.get_value("Save Scene"):
+            self.scene_manager.store()
+
+        if self.ui.get_value("Load Scene"):
+            self.scene_manager.load()
+            self.physics_ui.regenerate()
